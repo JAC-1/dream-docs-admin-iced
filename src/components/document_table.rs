9590e631b82ece1bc923498552c17aaeed5d0228
JAC-1 <79427501+JAC-1@users.noreply.github.com>
2025-02-05 08:08:31 +0900

    fixing student profile

--- src/components/document_table.rs
+++ src/components/document_table.rs
@@ -1,58 +1,64 @@
 use crate::models::supabase_models::*;
 use crate::types::FileStatus;
 use crate::Message;
-use iced::advanced::graphics::core::font;
-use iced::widget::{button, column, pick_list, rich_text, row, scrollable, span, text};
-use iced::{Element, FillPortion, Font};
+use iced::widget::{button, column, pick_list, row, scrollable, span, text, Container};
+use iced::{Center, Element, Fill, FillPortion};
 
 pub fn student_documents_table(docs: &Vec<File>) -> Element<'static, Message> {
-    let header_text = |text: String| {
-        rich_text([span(text).font(Font {
-            family: iced::font::Family::Name("Noto Sans Jp"),
-            weight: font::Weight::Bold,
-            ..Default::default()
-        })])
-        .width(FillPortion(1))
-    };
-
-    let document_header = row![
-        header_text("Document ID".to_string()),
-        header_text("Task Type".to_string()),
-        header_text("Status".to_string()),
-        header_text("Mime Type".to_string()),
-        header_text("Status Message".to_string()),
-        header_text("Created At".to_string()),
-        header_text("Updated At".to_string()),
-        header_text("Download".to_string())
-    ];
-
-    let document_row = |doc: &File| {
-        row![
-            text(doc.document_id.clone()).width(FillPortion(1)), // Document ID
-            text(doc.task_type.to_str().to_string()).width(FillPortion(1)), // Task Type
-            status_indicator(doc.status.clone(), doc.document_id.clone()), // Status
-            text(doc.mime_type.clone()).width(FillPortion(1)),   // Mime Type
-            text(doc.status_message.clone().unwrap_or_default()).width(FillPortion(1)), // Status Message
-            text(doc.created_at.to_string()).width(FillPortion(1)), // Created At
-            text(doc.updated_at.to_string()).width(FillPortion(1)), // Updated At
-            button("Download")
-                .width(FillPortion(1))
-                .on_press(Message::FetchStudentDoc(doc.clone()))
-        ]
-    };
-
     let download_all_button = button("Download All").on_press(Message::DownloadAllDocs);
-    let main_container = column![download_all_button];
-    let mut main_container = main_container.push(document_header);
+    let mut main_container = column![download_all_button];
     let mut docs_container = column![];
 
     for doc in docs.clone() {
-        let doc_row = document_row(&doc);
-        docs_container = docs_container.push(doc_row);
+        let mut count = 0;
+        let mut doc_row = row![];
+        if count == 3 {
+            docs_container = docs_container.push(doc_row);
+            doc_row = row![];
+            count = 0;
+        } else {
+            let doc_card = document_card(&doc);
+            doc_row = doc_row.push(doc_card);
+            count = count + 1;
+        }
     }
     let scrollable_docs = scrollable(docs_container);
-    main_container = main_container.push(scrollable_docs);
+    let main_container = main_container.push(scrollable_docs);
     main_container.into()
+}
+
+fn document_card(doc_info: &File) -> Element<'static, Message> {
+    let date = doc_info
+        .updated_at
+        .clone()
+        .format("Y - %m - %d")
+        .to_string();
+    let doc_name = text(doc_info.file_name.clone()).size(20);
+    let date = text(date).size(18);
+    let status = status_indicator(doc_info.status.clone(), doc_info.document_id.clone());
+    let download = button(text!("Download").size(12).width(Fill).center())
+        .width(Fill)
+        .on_press(Message::FetchStudentDoc(doc_info.clone()));
+
+    let doc_info_column = column![doc_name, date, status].align_x(Center);
+
+    let main_doc_column = column![doc_info_column, download]
+        .padding(21)
+        .spacing(10)
+        .align_x(Center);
+
+    Container::new(main_doc_column)
+        .max_height(188)
+        .height(188)
+        .width(152)
+        .max_width(152)
+        .style(|_| iced::widget::container::Style {
+            background: Some(iced::Background::Color(iced::Color::from_rgb8(
+                248, 248, 248,
+            ))),
+            ..Default::default()
+        })
+        .into()
 }
 
 fn status_indicator(status: FileStatus, document_id: String) -> Element<'static, Message> {
